065db976ec8ca2f9f9a2b5569b7569d791f0fcf7
Крутьков Александр Евгеньевич <s25380310@unn.ru>
2026-09-07 14:10:39 +0300

    sample_numbers

--- samples/sample_prime_numbers.cpp
+++ samples/sample_prime_numbers.cpp
@@ -13,6 +13,9 @@
 #ifndef USE_SET // Использовать класс TBitField
 
 #include "tbitfield.h"
+using std::cout;
+using std::endl;
+using std::cin;
 
 int main()
 {
@@ -43,7 +46,7 @@ int main()
     if (s.GetBit(m))
     {
       count++;
-      cout << setw(3) << m << " ";
+      cout << std::setw(3) << m << " ";
       if (k++ % 10 == 0)
         cout << endl;
     }
